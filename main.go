5a97a2b6afe9f9c8bacfd9ff5def66e013d8c041
zdf <2239432836@qq.com>
2023-08-09 18:38:21 +0800

    修改routes方式

--- main.go
+++ main.go
@@ -2,17 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/DeanThompson/ginpprof"
-	"github.com/gin-gonic/gin"
 	"log"
 	"toktik/conf"
-	"toktik/controller"
 	"toktik/dao/db"
 	"toktik/model"
+	"toktik/routes"
 )
 
 func main() {
-	router := gin.Default()
 	// 初始化配置文件
 	err := conf.InitConfig()
 	if err != nil {
@@ -29,7 +26,11 @@ func main() {
 	if err != nil {
 		log.Fatal("迁移数据库失败:", err)
 	}
-	ginpprof.Wrapper(router)
-	router.GET("/douyin/feed/", controller.GetFeed)
-	_ = router.Run(":8000")
+	//注册路由
+	r := routes.InitRouter()
+	// 启动8000端口
+	errRun := r.Run(":8000")
+	if errRun != nil {
+		return
+	}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"toktik/controller"
+)
+
+func InitRouter() *gin.Engine {
+	r := gin.Default()
+	// 主路由组
+	douyinGroup := r.Group("/douyin")
+	{
+		// feed视频流
+		douyinGroup.GET("/feed/", controller.GetFeed)
+	}
+	return r
+}
